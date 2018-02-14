3e269e3b53244be4531c148c71a481a15d305221
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-02-13 21:26:49 -0500

    raptorgit status

--- src/com/revature/Raptor.java
+++ src/com/revature/Raptor.java
@@ -5,7 +5,7 @@ public class Raptor extends Bird {
 	static { System.out.println("r1");}
 	public Raptor() {
 		//super()
-		System.out.println("r2");
+		System.out.println("RAPTOR!!");
 	}
 	{System.out.println("r3");}
 	static{System.out.println("r4");}
