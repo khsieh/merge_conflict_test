681cd2099ec090c3522e4e66074aad9c33b7242e
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-02-14 09:20:55 -0500

    Raptor!

--- src/com/revature/Raptor.java
+++ src/com/revature/Raptor.java
@@ -4,7 +4,7 @@ public class Raptor extends Bird {
 	static { System.out.println("r1");}
 	public Raptor() {
 		//super()
-		System.out.println("r2");
+		System.out.println("Raptor!");
 	}
 	{System.out.println("r3");}
 	static{System.out.println("r4");}
