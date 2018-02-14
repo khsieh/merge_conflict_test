e579f529734509172b3e1f3b2988e44c0d5fa7c3
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-02-13 21:08:56 -0500

    init commit

---
+++ src/com/revature/Raptor.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+
+public class Raptor extends Bird {
+	static { System.out.println("r1");}
+	public Raptor() {
+		//super()
+		System.out.println("r2");
+	}
+	{System.out.println("r3");}
+	static{System.out.println("r4");}
+}
