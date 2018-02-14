b97758d92f7a26f20714d3ec746963ef6e36bab3
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-02-14 09:26:41 -0500

    hawk!

--- src/com/revature/Hawk.java
+++ src/com/revature/Hawk.java
@@ -4,6 +4,7 @@ public class Hawk extends Raptor {
 	public static void main(String[] args) {
 		System.out.println("init");
 		new Hawk();
+		new Hawk();
 		System.out.println("hawk");
 	}
 	public Hawk() {
